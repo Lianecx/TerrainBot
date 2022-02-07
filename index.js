40417d5e5a61e5a4da8eb97ddf532d44ee084698
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-02-07 19:54:20 +0100

    Added `bree` job

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const Discord = require('discord.js');
+const Bree = require('bree');
 const { token, apikey } = require('./config.json');
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
 
+const bree = new Bree({
+    jobs: [
+        {
+            name: 'updateSubs',
+            interval: 'every week'
+        },
+    ],
+});
+
 client.once('ready', async () => {
     console.log(`Bot logged in as ${client.user.tag}.`);
     client.user.setActivity('over TheTerrain', { type: 'WATCHING' });
+    bree.start();
 });
 
 client.login(token);
