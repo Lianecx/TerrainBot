0e9a39002e4bad7cd32466c299190592bc045368
TheAnnoying <drork2018@gmail.com>
2022-02-07 21:37:14 +0200

    Now updates the test channel

--- functions.js
+++ functions.js
@@ -1,9 +1,12 @@
 const fetch = require('node-fetch');
+const client = require("./index.js");
+
 function getSubs(user, key){
     fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`).then(response => { 
         return response.json() 
     }).then(data => { 
-        console.log(parseInt(data["items"][0].statistics.subscriberCount).toLocaleString());
+        let subCount = parseInt(data["items"][0].statistics.subscriberCount).toLocaleString();
+        client.channels.cache.get("940328103455125565").setName(`👀 Subscribers: ${subCount}`);
     });
 }
 
