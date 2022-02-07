0590e6a7c5f9e2e9f60f9cf3621cfdd3a276c3f4
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-02-07 20:35:50 +0100

    Cleared functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,1 @@
-const fetch = require('node-fetch');
-function getSubs(user, key){
-    fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`).then(response => { 
-        return response.json() 
-    }).then(data => { 
-        console.log(parseInt(data["items"][0].statistics.subscriberCount).toLocaleString());
-    });
-}
 
-module.exports = {getSubs}
--- jobs/update_subs.js
+++ jobs/update_subs.js
@@ -1,9 +1,9 @@
 const { apikey } = require('../config.json');
 const fetch = require('node-fetch');
 
-const youtubeUser = 'UCLlVS9RRl8nC2Yirs1YbrOg'; //TheTerrains youtube ID
+const youtubeId = 'UCLlVS9RRl8nC2Yirs1YbrOg'; //TheTerrains youtube ID
 
-fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${apikey}`)
+fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeId}&key=${apikey}`)
     .then(response => {
         return response.json()
     }).then(data => {
