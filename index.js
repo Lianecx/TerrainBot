aff1958f6013154163cd807618bd982ac3d237df
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-02-09 16:15:09 +0100

    Updated updateSubcount method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const cron = require('node-cron');
 const config = require('./config.json');
 
-const { updateSubs, subCount } = require('./updateSubs');
+const { updateSubcount } = require('./updateSubs');
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
 client.once('ready', async () => {
@@ -10,12 +10,8 @@ client.once('ready', async () => {
     client.user.setActivity('over TheTerrain', { type: 'WATCHING' });
 
     const subcountChannel = client.channels.cache.get(config.channels.subcount);
-    updateSubs(subcountChannel, config.youtubeId);
-    cron.schedule('0 */12 * * *', () => updateSubs(subcountChannel, config.youtubeId)); //Update subs every 12 hours
-});
-
-client.on("messageCreate", async(message) => {
-    if(message.content === "t!subcount") return message.reply({ embeds: [ new Discord.MessageEmbed().setTitle("TheTerrain's subscriber count").setDescription(`TheTerrain's current subscriber count is \`${await subCount(config.youtubeId)}\``).setColor("#f1c40f")]}); 
+    updateSubcount(subcountChannel, config.youtubeId);
+    cron.schedule('0 */12 * * *', () => updateSubcount(subcountChannel, config.youtubeId)); //Update subs every 12 hours
 });
 
 client.login(config.token);
--- updateSubs.js
+++ updateSubs.js
@@ -1,21 +1,18 @@
 const fetch = require('node-fetch');
 const { apikey } = require('./config.json');
 
-async function updateSubs(channel, youtubeId) {
-    const resp = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeId}&key=${apikey}`);
-    const data = await resp.json();
-
-    const subs = data['items'][0].statistics.subscriberCount;
+async function updateSubcount(channel, youtubeId) {
+    const subs = getSubcount(youtubeId);
 
     channel.setName(`👀 Subscribers: ${subs / 1000}K`);
     console.log(`Updated sub count: ${subs / 1000}K`);
 }
 
-async function subCount(youtubeId) {
+async function getSubcount(youtubeId) {
     const resp = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeId}&key=${apikey}`);
     const data = await resp.json();
 
     return `${data['items'][0].statistics.subscriberCount / 1000}K`;
 }
 
-module.exports = { updateSubs, subCount };
+module.exports = { updateSubcount, getSubcount };
