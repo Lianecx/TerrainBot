462acf34612e083288faf8f29738f41fbdf059d3
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-02-09 14:08:00 +0100

    Added new updateSubs.js

---
+++ updateSubs.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+const { apikey } = require('./config.json');
+
+async function updateSubs(channel, youtubeId) {
+    const resp = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeId}&key=${apikey}`);
+    const data = await resp.json();
+
+    const subs = data['items'][0].statistics.subscriberCount;
+
+    channel.setName(`👀 Subscribers: ${subs / 1000}K`);
+    console.log(`Updated sub count: ${subs / 1000}K`);
+}
+
+module.exports = { updateSubs };
