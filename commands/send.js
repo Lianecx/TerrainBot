09cf094347b8e589311be9edf1a942743b975888
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-02-10 17:26:26 +0100

    Formatted builders in send command

--- commands/send.js
+++ commands/send.js
@@ -17,33 +17,56 @@ module.exports = {
             .addSubcommand(subcommand =>
                 subcommand.setName("channel")   
                 .setDescription("Send a message to a specific channel")
-                .addStringOption(option => option.setName("id").setRequired(true).setDescription("The ID of the channel to send the message to"))
-                .addStringOption(option => option.setName("message").setRequired(true).setDescription("The message to send"))
+                .addChannelOption(option =>
+                    option.setName("channel")
+                    .setRequired(true)
+                    .setDescription("The channel to send the message to"))
+                .addStringOption(option =>
+                    option.setName("message")
+                    .setRequired(true)
+                    .setDescription("The message to send"))
             ).addSubcommand(subcommand =>
                 subcommand.setName("user")
                 .setDescription("Send a message to a specific user")
-                .addStringOption(option => option.setName("id").setRequired(true).setDescription("The ID of the user to send the message to"))
-                .addStringOption(option => option.setName("message").setRequired(true).setDescription("The message to send"))
+                .addUserOption(option =>
+                    option.setName("user")
+                        .setRequired(true)
+                        .setDescription("The user to send the message to"))
+                .addStringOption(option =>
+                    option.setName("message")
+                        .setRequired(true)
+                        .setDescription("The message to send"))
             ),
     async execute(interaction, client) {
-        const id = interaction.options.getString("id");
+        const user = interaction.options.getUser("user");
+        const channel = interaction.options.getChannel("channel");
         const msg = interaction.options.getString("message");
 
-        if(interaction.options.getSubcommand() === "channel"){
+        if(interaction.options.getSubcommand() === "channel") {
             try {
-                client.channels.cache.get(id).send(`${msg}`);
+                await channel.send(msg);
             } catch(err) {
                 return interaction.reply({ embeds: [fn.sendError("Could not reach channel, the id might've been invalid or I do not have access to that channel")] });
             }
 
-            return interaction.reply({ embeds: [new Discord.MessageEmbed().setAuthor({ name: "Success!", iconURL: "https://cdn.discordapp.com/emojis/914130307362484265.webp?size=96&quality=lossless"}).setDescription(`Successfully sent \`${msg}\` to the ${client.channels.cache.get(id).name} channel`).setColor(config.colors.success)] });
-        } else if(interaction.options.getSubcommand() === "user"){
+            const successEmbed = new Discord.MessageEmbed()
+                .setAuthor({ name: "Success!", iconURL: "https://cdn.discordapp.com/emojis/914130307362484265.webp?size=96&quality=lossless"})
+                .setDescription(`Successfully sent \`${msg}\` to ${channel.name}`)
+                .setColor(config.colors.success);
+            return interaction.reply({ embeds: [successEmbed] });
+        } else if(interaction.options.getSubcommand() === "user") {
             try {
-                client.users.cache.get(id).send(`${msg}`)
+                user.send(`${msg}`);
             } catch(err){
                 return interaction.reply({ embeds: [fn.sendError("Could not reach user, the id might've been invalid or their dm's are not enbaled")] });
             }
-            return interaction.reply({ embeds: [new Discord.MessageEmbed().setAuthor({ name: client.user.tag, iconURL: client.user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 }) }).setDescription(msg).setFooter({ text: `Sent to ID: ${id}` }).setColor(bot.accentColor)], ephemeral: false });
+
+            const successEmbed = new Discord.MessageEmbed()
+                .setAuthor({ name: client.user.tag, iconURL: client.user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 }) })
+                .setDescription(msg)
+                .setFooter({ text: `Successfully sent \`${msg}\` to ${user.name}` })
+                .setColor(config.colors.success);
+            return interaction.reply({ embeds: [successEmbed], ephemeral: false });
         }
     }
 }
